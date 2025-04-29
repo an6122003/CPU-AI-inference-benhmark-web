bb0320580e6b626f9fcd78af6a36d7bfbf681b7e
An Nguyen <an6122003@gmail.com>
2025-04-29 08:50:42 +0700

    commit

--- test.sh
+++ test.sh
@@ -7,4 +7,8 @@ pip3 install --ignore-installed --break-system-packages Flask
 pip3 install gunicorn --break-system-packages
 pip install flask-cors --break-system-packages
 gunicorn --bind 0.0.0.0:5000 app:app
+# Run in background and save output to nohup.out (nohangup when terminal is closed)
 nohup gunicorn --bind 0.0.0.0:5000 app:app &
+
+## other models
+wget https://github.com/onnx/models/raw/main/validated/vision/classification/resnet/model/resnet152-v1-7.onnx
